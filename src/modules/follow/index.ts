2c955ae9ee906126c7dbb1a094ff06e42dbb71c0
nr1a <_@nr1a.inc>
2024-08-06 19:31:50 +0900

    test

--- src/modules/follow/index.ts
+++ src/modules/follow/index.ts
@@ -1,30 +1,36 @@
-import { bindThis } from '@/decorators.js';
-import Module from '@/module.js';
-import Message from '@/message.js';
+import { bindThis } from "@/decorators.js";
+import Module from "@/module.js";
+import Message from "@/message.js";
 
 export default class extends Module {
-	public readonly name = 'follow';
+	public readonly name = "follow";
 
 	@bindThis
 	public install() {
 		return {
-			mentionHook: this.mentionHook
+			mentionHook: this.mentionHook,
 		};
 	}
 
 	@bindThis
 	private async mentionHook(msg: Message) {
-		if (msg.text && msg.includes(['フォロー', 'フォロバ', 'follow me'])) {
+		const targetUserId = "@{userid}@omaka.nr1a.inc";
+
+		if (
+			msg.text &&
+			msg.includes(["フォロー", "フォロバ", "follow me"]) &&
+			msg.userId === targetUserId
+		) {
 			if (!msg.user.isFollowing) {
-				this.ai.api('following/create', {
+				this.ai.api("following/create", {
 					userId: msg.userId,
 				});
 				return {
-					reaction: msg.friend.love >= 0 ? 'like' : null
+					reaction: msg.friend.love >= 0 ? "like" : null,
 				};
 			} else {
 				return {
-					reaction: msg.friend.love >= 0 ? 'hmm' : null
+					reaction: msg.friend.love >= 0 ? "hmm" : null,
 				};
 			}
 		} else {
